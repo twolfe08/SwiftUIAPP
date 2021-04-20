39f4a5a44a96e7538371e8a50acf5d9f8584ad32
Tyesha <tyesha.wolfe@yahoo.com>
2021-04-20 16:16:50 -0400

    Initial Commit

---
+++ SwiftUIAPP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIAPP
+//
+//  Created by Tyesha Wolfe-David on 4/20/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIAPP/SwiftUIAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAPPApp.swift
+//  SwiftUIAPP
+//
+//  Created by Tyesha Wolfe-David on 4/20/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
